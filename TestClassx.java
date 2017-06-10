c685a44b75590f8b290e372f3883e7d871b15b58
manee2k6 <manee2k6@gmail.com>
2017-06-10 17:06:31 +0530

    Adding the source to github

---
+++ TestClassx.java
@@ -0,0 +1,12 @@
+package com.devsmart.practise;
+
+public class TestClassx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to DevOps");
+
+	}
+
+}
